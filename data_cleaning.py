1d206d659ee3668b4c60f03e41fb848711424c56
be-ns <benjaminsiverly@gmail.com>
2017-08-22 11:08:37 -0600

    updated script and cleaned data

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,32 +8,51 @@ import os
 import spacy
 
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer as tfidf
+from string import punctuation as punc
 
 
 def check_df(path, nlp):
     df = pd.read_csv(path)
-    df.columns = ['sent', 'type']
+    df.columns = ['sent', 'label']
     df = remove_stop_words(df, nlp)
     return df
 
 def remove_stop_words(df, nlp):
-    df.sent = df.sent.apply(lambda word_list: _check_for_stops(word_list))
-    df.sent = df.sent.apply(lambda sentence: lemmatize_with_spacy(sentence, nlp))
+    df.sent = df.sent.apply(lambda words: _check_for_stops(words))
+    df.sent = df.sent.apply(lambda words: lemmatize_with_spacy(words, nlp))
     return df
 
 def _check_for_stops(sent):
     d = enchant.Dict('en_US')
-    return re.sub("[0-9]", "", ' '.join([word for word in list(sent.split(' ')) if word not in set(stopwords.words('english')) and d.check(word) == True]))
+    return re.sub("[0-9]", "", ' '.join([word.lower().strip(punc) for word in list(sent.split(' ')) if word not in set(stopwords.words('english')) and d.check(word) == True]))
 
 def lemmatize_with_spacy(sent, nlp):
-    sent1 = sent.split(' ')
-    sent2 = nlp(sent)
-    for ix, token in enumerate(sent2):
-        sent1[ix] = token.lemma_
-    return ' '.join(sent1)
+    replacement, originals = nlp(sent), []
+    for ix, token in enumerate(replacement):
+        originals.append(token.lemma_)
+    return ' '.join(originals)
+
+def get_tfidf(df):
+    v = tfidf()
+    x = v.fit_transform(df['sent']).to_dense()
+    print(type(x))
+    return df
+
+# remove punctuation
+# read in data from the cleaned_data.csv file
+# change to_dense to make sparse matrix into np array and add into pandas df with labels - save that as tfidt_data.csv
+# combine that all into one function with hidden methods
+
 
 if __name__ == '__main__':
-    #libc.load_data()
+    # completed initial load
+    libc.load_data()
+    # completed initial cleaning
     nlp = spacy.load('en')
     df = check_df('data.csv', nlp)
+    # df = get_tfidf(df)
+    # df = strip_punctuation(df)
+    print(df.columns.tolist())
+    df.to_csv(path_or_buf='cleaned_data.csv', header=['sent', 'label'])
     print(df.head(2))
